64a721958d752325d71f8b33c263649fed1cacf1
Air <baishuiz@gmail.com>
2018-09-05 09:08:19 +0800

    初始化时，校验目录是否已经存在

--- src/init.js
+++ src/init.js
@@ -6,9 +6,14 @@ var fsCopy = require("./util/fsCopy.js");
 
 
 module.exports = function (appName) {
-    fsCopy.mkdir(appName);
-    var templatePath = path.resolve(__dirname, "../template/0");
-    fsCopy.copyDir(templatePath, appName);
-    // fs.unlinkSync('/tmp/hello');
+    if(fs.existsSync(appName)){
+        fsCopy.mkdir(appName);
+        var templatePath = path.resolve(__dirname, "../template/0");
+        fsCopy.copyDir(templatePath, appName);
+        // fs.unlinkSync('/tmp/hello');
+    } else {
+        console.warn("project folder" + [appName] + "is exists.")
+    }
+    
     
 };
