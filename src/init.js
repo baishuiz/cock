5dd645622143ef7c7aa96c6adfa427f7874a31ed
Air <baishuiz@gmail.com>
2018-09-05 09:18:23 +0800

    fix bug: init appName exists

--- src/init.js
+++ src/init.js
@@ -6,7 +6,7 @@ var fsCopy = require("./util/fsCopy.js");
 
 
 module.exports = function (appName) {
-    if(fs.existsSync(appName)){
+    if(!fs.existsSync(appName)){
         fsCopy.mkdir(appName);
         var templatePath = path.resolve(__dirname, "../template/0");
         fsCopy.copyDir(templatePath, appName);
